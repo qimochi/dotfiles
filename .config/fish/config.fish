5499d8054e2cac219da5dab6d1d24c3eba28bc41
qimochi <89045657+qimochi@users.noreply.github.com>
2021-08-17 05:15:30 +0700

    Create config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+GNU nano 5.8 /home/xx/.config/fish/config.fish
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+source ~/.local/share/omf/pkg/colorman/init.fish
+
+set -x FFF_W3M_XOFFSET "200"
+set -x FFF_W3M_YOFFSET "200"
+set -x FFF_HIDDEN "1"
+set -x EDITOR "vim"
+set -x CPR_LIB "~/cpr"
+set -x pap_DIR "~/pap/palettes/"
