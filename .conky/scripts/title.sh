1ff542d8171f7fecd69eee0edc4a61a37eb139cd
qimochi <89045657+qimochi@users.noreply.github.com>
2021-08-18 11:16:42 +0700

    for title

---
+++ .conky/scripts/title.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+title=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
+if [ ${#title} -ge 35 ] 
+then
+	echo ${title:0:30}"..."
+else
+	echo $title
+fi
